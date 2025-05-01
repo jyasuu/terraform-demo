69e588e2d88a5c3efa05a4ab6ae40b26ce511869
jyasuu <s1002605@pu.edu.tw>
2025-05-01 11:32:47 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,3 +61,103 @@ resource "kubernetes_service" "test" {
     }
   }
 }
+
+
+resource "kubernetes_namespace" "demo" {
+  metadata {
+    name = "demo"
+  }
+}
+
+resource "kubernetes_deployment" "whoami" {
+  metadata {
+    name      = "whoami"
+    namespace = kubernetes_namespace.demo.metadata[0].name
+    labels = {
+      app = "whoami"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "whoami"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "whoami"
+        }
+      }
+
+      spec {
+        container {
+          name  = "whoami"
+          image = "traefik/whoami"
+
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "whoami" {
+  metadata {
+    name      = "whoami"
+    namespace = kubernetes_namespace.demo.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.whoami.metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_ingress_v1" "whoami" {
+  metadata {
+    name      = "whoami"
+    namespace = kubernetes_namespace.demo.metadata[0].name
+    annotations = {
+      "traefik.ingress.kubernetes.io/router.entrypoints" = "web"
+    }
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+
+    rule {
+      host = var.host
+
+      http {
+        path {
+          path     = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = kubernetes_service.whoami.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
