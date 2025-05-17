50813777bd327ef01744f381478389fd19dc456d
jyasuu <s1002605@pu.edu.tw>
2025-05-17 06:08:30 +0000

    try pod

--- main.tf
+++ main.tf
@@ -1,1 +1,39 @@
+resource "kubernetes_namespace" "pod-demo" {
+  metadata {
+    name = "pod-demo"
+  }
+}
 
+resource "kubernetes_pod" "whoami" {
+  metadata {
+    namespace = kubernetes_namespace.pod-demo.metadata[0].name
+    name      = whoami
+    labels = {
+      app = whoami
+    }
+
+  }
+  spec {
+    container {
+      name  = whoami1
+      image = traefik / whoami
+      port {
+        container_port = 80
+      }
+      port {
+        container_port = 443
+      }
+    }
+    container {
+      name  = whoami2
+      image = traefik / whoami
+      port {
+        container_port = 80
+      }
+      port {
+        container_port = 443
+      }
+
+    }
+  }
+}
