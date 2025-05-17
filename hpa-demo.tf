3af5f58ccd737c2a921452dbb70288b48f2f1b0c
jyasuu <s1002605@pu.edu.tw>
2025-05-17 13:49:44 +0000

    try hpa

---
+++ hpa-demo.tf
@@ -0,0 +1,98 @@
+resource "kubernetes_namespace" "hpa-demo" {
+  metadata {
+    name = "hpa-demo"
+  }
+}
+
+
+resource "kubernetes_deployment" "hpa_demo" {
+  metadata {
+    name = "hpa-demo"
+    namespace = kubernetes_namespace.hpa-demo.metadata[0].name
+    labels = {
+      app = "hpa-demo"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "hpa-demo"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "hpa-demo"
+        }
+      }
+
+      spec {
+        container {
+          name  = "hpa-demo"
+          image = "traefik/whoami"
+          port {
+            container_port = 80
+          }
+
+          resources {
+            requests = {
+              cpu = "100m"
+            }
+            limits = {
+              cpu = "200m"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "hpa_demo" {
+  metadata {
+    namespace = kubernetes_namespace.hpa-demo.metadata[0].name
+    name = "hpa-demo"
+  }
+  spec {
+    selector = {
+      app = "hpa-demo"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "hpa" {
+  metadata {
+    name = "hpa-demo-hpa"
+  }
+
+  spec {
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = "hpa-demo"
+    }
+
+    min_replicas = 1
+    max_replicas = 5
+
+    metric {
+      type = "Resource"
+      resource {
+        name = "cpu"
+        target {
+          type                = "Utilization"
+          average_utilization = 50
+        }
+      }
+    }
+  }
+}
---
+++ hpa.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '1m', target: 100 }, // Ramp-up to 100 VUs
+    { duration: '3m', target: 100 }, // Sustain load
+    { duration: '1m', target: 0 },   // Ramp-down
+  ],
+};
+//  port-forward svc/hpa-demo 8080:80
+export default function () {
+  const res = http.get('http://localhost:8080');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
