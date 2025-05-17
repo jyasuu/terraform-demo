3056fdbcbb071a69744af15780bcf7a705a399d0
jyasuu <s1002605@pu.edu.tw>
2025-05-17 13:59:56 +0000

    remove service

--- main.tf
+++ main.tf
@@ -40,26 +40,3 @@ resource "kubernetes_pod" "whoami" {
   }
 }
 
-
-resource "kubernetes_service" "whoami-svc" {
-  metadata {
-    namespace = kubernetes_namespace.pod-demo.metadata[0].name
-    name      = "whoami"
-    labels = {
-      "key" = "value"
-    }
-  }
-  spec {
-    selector = {
-      "app" = "whoami"
-    }
-    type = "LoadBalancer"
-    port {
-      protocol    = "TCP"
-      port        = 80
-      target_port = 80
-    }
-
-  }
-
-}
