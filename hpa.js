36ff1e573021e2bedd58520bc26e6c99dff7598f
jyasuu <s1002605@pu.edu.tw>
2025-05-17 14:10:19 +0000

    comment

--- hpa.js
+++ hpa.js
@@ -8,7 +8,8 @@ export let options = {
     { duration: '1m', target: 0 },   // Ramp-down
   ],
 };
-//  port-forward svc/hpa-demo 8080:80
+// kubectl port-forward svc/hpa-demo 8080:80
+// kubectl get hpa -n hpa-demo  && kubectl get pods -n hpa-demo 
 export default function () {
   const res = http.get('http://localhost:8080');
   check(res, { 'status was 200': (r) => r.status == 200 });
